d1e3e08d667328567a0a9cf9d105692867ab7f0f
Bingbing Huang <Bingbing.Huang@BYTON.COM>
2021-03-12 17:12:08 +0800

    服务链路追踪监控

---
+++ src/test/java/com/hbb/demo/sleuth_zipkin/SleuthZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hbb.demo.sleuth_zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthZipkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
