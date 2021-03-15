876e1f0afe97ddb7c1a4a55082016ea6feeafb3e
bingbing.huang <znjt126@126.com>
2021-03-15 15:02:42 +0800

    # 配置sleuth+zipkin

---
+++ service3/src/main/java/Service3Application.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+
+@SpringCloudApplication
+public class Service3Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Service3Application.class, args);
+    }
+}
---
+++ service3/src/main/java/com/hbb/demo/service3/controller/ServiceController.java
@@ -0,0 +1,21 @@
+package com.hbb.demo.service3.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "service3")
+public class ServiceController {
+
+    @RequestMapping(value = "f1")
+    @ResponseBody
+    public Map<String, String> service1Fun1() {
+        Map map = new HashMap();
+        map.put("id", "1");
+        return map;
+    }
+}
